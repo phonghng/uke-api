0bc34b965efe59676a296269e5a16ed0e40319db
phonglan123 <32542949+phonglan123@users.noreply.github.com>
2021-08-07 14:46:14 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const timeout = require('connect-timeout')
+const timeout = require('connect-timeout');
+const cors = require('cors')
 
 const app = express();
 
@@ -14,30 +15,30 @@ var corsOptions = {
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
-app.get('/', cors(corsOptions), timeout('4444s'), function(req, res, next) {
+app.get('/', cors(corsOptions), timeout('30s'), function(req, res, next) {
     res.writeHead(302, {
         Location: "https://www.npmjs.com/package/phonghng-url-kw-extractor"
     });
     res.end();
 });
 
-app.get('/supportlang', cors(corsOptions), timeout('4444s'), function(req, res, next) {
+app.get('/supportlang', cors(corsOptions), timeout('30s'), function(req, res, next) {
     res.json({ "support-lang": uke.support_lang });
 }); 
 
-app.post('/kwurl', cors(corsOptions), timeout('4444s'), function(req, res, next) {
+app.post('/kwurl', cors(corsOptions), timeout('30s'), function(req, res, next) {
     uke.get_keyword(req.body.url, keywords => {
         res.json(keywords);
     });
 });
 
-app.post('/kwhtml', cors(corsOptions), timeout('4444s'), function(req, res, next) {
+app.post('/kwhtml', cors(corsOptions), timeout('30s'), function(req, res, next) {
     uke.get_keyword_from_html(req.body.html, keywords => {
         res.json(keywords);
     });
 });
 
-app.post('/kwstring', cors(corsOptions), timeout('4444s'), function(req, res, next) {
+app.post('/kwstring', cors(corsOptions), timeout('30s'), function(req, res, next) {
     uke.get_keyword_from_string(req.body.string, keywords => {
         res.json({ "keywords": keywords });
     });
