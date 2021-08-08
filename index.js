ce1461dbb16d7bd01a2a7bdf1bfcad80f731c9fe
Hải Phong Nguyễn <phonghng8420@gmail.com>
2021-08-08 13:45:45 +0700

    1

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const uke = require('phonghng-url-kw-extractor')
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 const PORT = process.env.PORT || 5000
 
 express()
+    .use(logger('dev'))
+    .use(express.json())
+    .use(express.urlencoded({ extended: false }))
+    .use(cookieParser())
     .get('/', (req, res) => {
         res.writeHead(302, {
             Location: "https://www.npmjs.com/package/phonghng-url-kw-extractor"
