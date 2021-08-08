4e6a5207a710885fe766fa187e45156edb168710
Hải Phong Nguyễn <phonghng8420@gmail.com>
2021-08-08 13:29:40 +0700

    Fix

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+    .get('/', (req, res) => {
+        res.writeHead(302, {
+            Location: "https://www.npmjs.com/package/phonghng-url-kw-extractor"
+        });
+        res.end();
+    })
+    .post('/kwurl', async function(req, res) {
+        try {
+            uke.get_keyword(req.body.url, false, keywords => res.json(keywords));
+        } catch (error) {
+            console.error(error);
+        }
+    })
+    .post('/kwhtml', async function(req, res) {
+        try {
+            uke.get_keyword_from_html(req.body.html, false, keywords => res.json(keywords));
+        } catch (error) {
+            console.error(error);
+        }
+    })
+    .post('/kwstring', async function(req, res) {
+        try {
+            uke.get_keyword_from_string(req.body.string, keywords => res.json(keywords));
+        } catch (error) {
+            console.error(error);
+        }
+    })
+    .post('/kwurl-twowords', async function(req, res) {
+        try {
+            uke.get_keyword(req.body.url, false, keywords => {
+                var filtered = Object.keys(keywords)
+                    .filter(key => key.split(" ").length <= 2)
+                    .reduce((obj, key) => {
+                        obj[key] = keywords[key];
+                        return obj;
+                    }, {});
+                res.json(filtered)
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    })
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
